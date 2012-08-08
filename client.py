c4ad729728b742776f30ad004f9c990a73820fa0
Fennec89 <fennecfox89@gmail.com>
2012-08-08 20:54:09 +0200

    added funtions to the server and client

--- client.py
+++ client.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python2
 # -*- coding: utf8 -*-
+from socket import *
 
+HOST = 'localhost'
+PORT = 29600
+ADDR = (HOST,PORT)
+BUFSIZE = 4096
 
+cli = socket( AF_INET,SOCK_STREAM)
+cli.connect((ADDR))
 
+for i in range(1, 200):
+    cli.send(str(i) + ", ")
 
+data = cli.recv(BUFSIZE)
+cli.close()
+
+print "Recieved data: ", data
 
 # vim: ts=4 et tw=79 cc=+1
--- server.py
+++ server.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python2
 # -*- coding: utf8 -*-
+from socket import *
 
+HOST = ''
+PORT = 29600
+ADDR = (HOST,PORT)
+BUFSIZE = 4096
 
+serv = socket( AF_INET,SOCK_STREAM)
 
+serv.bind((ADDR))
+serv.listen(1)
+print "listening..."
 
+conn,addr = serv.accept()
+print "...connected: ", addr
+while 1:
+    data = conn.recv(BUFSIZE)
+    if not data: break
+    print "recived data: ", data
+    conn.send(data)
+conn.close()
 
 # vim: ts=4 et tw=79 cc=+1
