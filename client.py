9ccecbf7828d01e78fe41aac1b733d9950050232
Fennec89 <fennecfox89@gmail.com>
2012-08-08 22:20:46 +0200

    The server can now communicate and send to the client and vice versa

--- client.py
+++ client.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python2
 # -*- coding: utf8 -*-
 from socket import *
+import time
+import sys
 
 HOST = 'localhost'
 PORT = 29600
 ADDR = (HOST,PORT)
 BUFSIZE = 4096
-
+MESSAGE = ['0','TO THE BOARD'] #first element: 1 = recive clipboard, 0 = paste to server
+MSG = ['1', str(sys.argv[1])] # second element defines how many lines to get from CB
 cli = socket( AF_INET,SOCK_STREAM)
 cli.connect((ADDR))
 
-for i in range(1, 200):
-    cli.send(str(i) + ", ")
+for i in range(0,4):
+    MESSAGE[1] = MESSAGE[1]+str(i)
+    print MESSAGE
+    cli.send(repr(MESSAGE))
+    time.sleep(2)
+time.sleep(2)
+cli.send(repr(MSG))
+data = eval(cli.recv(BUFSIZE))
 
-data = cli.recv(BUFSIZE)
 cli.close()
 
 print "Recieved data: ", data
--- server.py
+++ server.py
@@ -7,19 +7,34 @@ PORT = 29600
 ADDR = (HOST,PORT)
 BUFSIZE = 4096
 
-serv = socket( AF_INET,SOCK_STREAM)
+clipboard = []
 
+serv = socket( AF_INET,SOCK_STREAM)
 serv.bind((ADDR))
 serv.listen(1)
+
 print "listening..."
 
 conn,addr = serv.accept()
 print "...connected: ", addr
+
 while 1:
     data = conn.recv(BUFSIZE)
+
     if not data: break
-    print "recived data: ", data
-    conn.send(data)
+    original_data = eval(data)
+
+    if original_data[0] == "0":
+        clipboard.append(original_data[1])
+        print "Current clipboard: ", clipboard
+    elif original_data[0] == "1":
+        print original_data[1]
+        index = original_data[1]
+        print clipboard[:int(index)]
+        print "sending data..."
+
+        conn.send(repr(clipboard[:int(index)]))
+
 conn.close()
 
 # vim: ts=4 et tw=79 cc=+1
